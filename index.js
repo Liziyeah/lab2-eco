bc993d01633afaac929b12ee95b991209388e70b
Mariana <marianitalizcanor@gmail.com>
2025-02-19 19:06:27 -0500

    fetch publish post

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-window.addEventListener('DOMContentLoaded', fetchData);
 
 async function fetchData() {
   try {
-    const response = await fetch("http://localhost:3004/posts");
+    const response = await fetch("http://localhost:3004/posts", {
+      
+    });
+    console.log("Respuesta de fetch:", response);
+    
 
     if (!response.ok) {
       throw new Error("Error en la red: " + response.status);
@@ -16,7 +19,6 @@ async function fetchData() {
     console.error("Error al obtener los datos:", error);
   }
 }
-
 
 const renderPosts = (arrayData) => {
 
@@ -32,6 +34,38 @@ const renderPosts = (arrayData) => {
         sectionPosts.appendChild(postCards);
     });
 }
+
+const addNewData = async () => {
+  let newPost = {
+    imageUrl: document.getElementById('post-url').value,
+    img: document.getElementById('post-title').value,
+    body: document.getElementById('post-description').value
+  }
+  try {
+    const response = await fetch("http://localhost:3004/posts", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newPost) 
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al agregar el post: " + response.status);
+    }
+
+    fetchData();
+  }catch(error){
+    console.error('No se pudo agregar el post '+ error);
+    
+  }
+}
+
+const publishPost = document.getElementById('publish-post');
+publishPost.addEventListener("click", addNewData);
+window.addEventListener('DOMContentLoaded', fetchData);
+
+
 
 
 
